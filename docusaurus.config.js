9033d2fe1ac75cbdb042e8e8da491a412d3f422e
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2022-08-31 11:38:44 +0900

    Modify sidebar and last update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,30 +14,31 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'github.com/oogasawa', // Usually your GitHub org/user name.
-    projectName: 'sau_Research00', // Usually your repo name.
-    plugins: [[require.resolve('docusaurus-lunr-search'), {
-        languages: ['ja', 'en']
-    }]],
-    i18n: {
-        defaultLocale: 'ja',
-        locales: ['ja', 'en'],
-    },
+  projectName: 'sau_Research00', // Usually your repo name.
+  plugins: [[require.resolve('docusaurus-lunr-search'), {
+    languages: ['ja', 'en']
+  }]],
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja', 'en'],
+  },
   presets: [
     [
       '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-            sidebarPath: require.resolve('./sidebars.js'),
-            routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          showLastUpdateTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/oogasawa/sau_Research00/edit/main/website/',
+          // editUrl: 'https://github.com/oogasawa/sau_Template00/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          'https://github.com/facebook/docusaurus/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -47,80 +48,80 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-        colorMode: {
-            disableSwitch: true,
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  ({
+    colorMode: {
+      disableSwitch: true,
+    },
+    navbar: {
+      title: 'Template00',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/king_logo.png',
+      },
+      items: [
+        {
+          to: 'intro',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
         },
-      navbar: {
-        title: 'Research00',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/king_logo.png',
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {type: 'localeDropdown',
+         position: 'right',
         },
-        items: [
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
             {
-                to: 'sec_Docsaurus_1012_2021_71a8/c_Setup_1012_2021_4d8d/c_Setup_1012_2021_4d8d',
-            docId: 'c_Setup_1012_2021_4d8d',
-            position: 'left',
-            label: 'Documentation',
-          },
-            {to: '/blog', label: 'Blog', position: 'left'},
-            {type: 'localeDropdown',
-             position: 'right',
+              label: 'Tutorial',
+              to: '/docs/intro',
             },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  }),
 };
 
 module.exports = config;
